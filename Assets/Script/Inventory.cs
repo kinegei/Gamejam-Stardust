5fed4f0decb2633959af5d524342e1e6c6b3d29e
dosjos <j.skotterud@gmail.com>
2015-10-24 16:16:35 +0200

    La til inventory og stardust med pickup

---
+++ Assets/Script/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour
+{
+
+
+    public int SparklePower = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/StarDust.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarDust : MonoBehaviour {
+
+    public GameObject Self;
+    private Inventory inventory;
+
+    public int StarPowerAmount = 20;
+
+    void Start()
+    {
+        var obj = GameObject.Find("Player");
+        inventory = obj.GetComponent<Inventory>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(Self);
+            inventory.SparklePower += StarPowerAmount;
+        }
+    }
+}
